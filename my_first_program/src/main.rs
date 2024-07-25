824d11ed8c5e9e3514b2c69b5ce27f716983ed21
Kevin <144179547+RologiO@users.noreply.github.com>
2024-07-25 21:32:26 +0000

    In Class Assignment

--- my_first_program/src/main.rs
+++ my_first_program/src/main.rs
@@ -1,3 +1,33 @@
+use std::process::Command;
+use std::io::{self, Write};
+
+fn executing_os_commands_linux(command_full: &str) {
+
+    let parts: Vec<&str> = command_full.split_whitespace().collect();
+    let actual_command = parts[0];
+    let arg1 = parts[1];
+
+    let output = Command::new(actual_command)
+        .arg(arg1)
+        .output()
+        .unwrap();
+
+    //println!("{:?}", output);
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
+}
+
+fn accept_linux_command_from_user() -> String{
+    let mut buffer = String::new();
+
+    print!("Input Linux Command: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer.trim().to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let full_command:String = accept_linux_command_from_user();
+    
+    executing_os_commands_linux(&full_command);
 }
--- my_first_program/target/debug/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Hello, Kevin, to the Moon!");
-}
